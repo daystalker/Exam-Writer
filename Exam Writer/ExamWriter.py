9e4ce90b8e08c89a1b55a209ddecf80c44972bed
Harhon <hsgohon1@gmail.com>
2018-03-08 07:16:09 +0530

    Version 1.1

--- Exam Writer/ExamWriter.py
+++ Exam Writer/ExamWriter.py
@@ -1,32 +1,19 @@
 import pyautogui
 import random
 import time
-a="a.png"
-b="b.png"
-c="c.png"
-d="d.png"
 m=input("Enter No. of Questions:")
+a="src"
+a+=r"\\"
 for i in range(int(m)):
-    n=random.randint(1,4)
-    print(n)
-    if(n==1):
-        x,y=pyautogui.locateCenterOnScreen(a)
-        pyautogui.moveTo(x-20,y)
-        pyautogui.click()
-    elif(n==2):
-        x,y=pyautogui.locateCenterOnScreen(b)
-        pyautogui.moveTo(x-20,y)
-        pyautogui.click()
-    elif(n==3):
-        x,y=pyautogui.locateCenterOnScreen(c)
-        pyautogui.moveTo(x-20,y)
-        pyautogui.click()
-    else:
-        x,y=pyautogui.locateCenterOnScreen(d)
-        pyautogui.moveTo(x-20,y)
-        pyautogui.click()
-    x,y=pyautogui.locateCenterOnScreen("N.png")
+    b=random.choice("abcd")
+    print(b)
+    b+=r".png"
+    c=a+b
+    x,y=pyautogui.locateCenterOnScreen(c)
+    pyautogui.moveTo(x-20,y)
+    pyautogui.click()
+    x,y=pyautogui.locateCenterOnScreen(r"src\N.png")
     pyautogui.moveTo(x,y)
     pyautogui.click()
-    time.sleep(10)
-
+    time.sleep(6)
+    
